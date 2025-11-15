ab08c30056309d4862d3c5ef53a4f9914f8c5c2d
gitusergb <g4ur131@gmail.com>
2025-11-15 20:54:20 +0530

    crmback

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,12 @@
  */
 const config = {
   api: {
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+    // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://crm-backend-u7wi.onrender.com/api',
   },
   socket: {
-    url: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000',
+    // url: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000',
+    url: import.meta.env.VITE_SOCKET_URL || 'https://crm-backend-u7wi.onrender.com',
   },
 };
 
